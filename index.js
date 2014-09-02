94c4d2a82d9f3bb2177009a0ee053b943ce9eaa0
imakewebthings <caleb@imakewebthings.com>
2014-09-01 20:38:57 -0700

    Tracer

--- index.js
+++ index.js
@@ -7,16 +7,26 @@ function trail(buffer, callback) {
     if (err) {
       return callback(err)
     }
+    var base = image.frames[0].data
     for (var i = 1; i < image.frames.length; i++) {
-      var frame = image.frames[i]
+      var frame = image.frames[i].data
+      var buf = new Buffer(base.length)
+      base.copy(buf)
+      for (var j = 0; j < buf.length; j+=4) {
+        buf[j] = (base[j] + frame[j]) / 2
+        buf[j+1] = (base[j+1] + frame[j+1]) / 2
+        buf[j+2] = (base[j+2] + frame[j+2]) / 2
+      }
+      image.frames[i].data = buf
+      base = buf
     }
     writegif(image, callback)
   })
 }
 
-var b = fs.readFileSync('/Users/calebtroughton/Desktop/test.gif')
+var b = fs.readFileSync('/Users/calebtroughton/Desktop/test2.gif')
 trail(b, function(err, tb) {
-  fs.writeFileSync('/Users/calebtroughton/Desktop/test-trail.gif', tb)
+  fs.writeFileSync('/Users/calebtroughton/Desktop/test-trail2.gif', tb)
   console.log('trailed')
 })
 
